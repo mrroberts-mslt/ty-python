d3426a793783b1e9141a5bff5ee6d1d96ff32c9a
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-10-06 20:15:04 +0100

    Create exercise1.py

---
+++ week2/exercise1.py
@@ -0,0 +1,7 @@
+your_name = "Alan"
+your_favourite_food = "pizza"
+something_youre_good_at = "table tennis"
+something_you_are = "cool"
+embarrassing_thing_you_did = "fell over in front of the president"
+nice_thing_you_do = "give to charity"
+print("Face it, "+ your_name + ", you are about the greatest\nthing since "+ your_favourite_food + ". No one else can "+ something_youre_good_at + "\nlike you can. Your best friend says you are\nthe "+ something_you_are + "-est person in the world! Sure, you\nonce "+ embarrassing_thing_you_did + ", but you also "+ nice_thing_you_do + " so\nreally, you are just great!")
