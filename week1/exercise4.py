cb3dd300bbb22cd5cb21a58f693c5d34a930a881
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-10-06 20:11:28 +0100

    Create exercise4.py

---
+++ week1/exercise4.py
@@ -0,0 +1,4 @@
+#1
+print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t*\t*\t*\t*\n\t\t*\t\t*PYTHON!*\t\t*\n\t*\t\t\t*\t\t*\t\t\t*\n*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*")
+#2
+print("\t\t\t\t******\n\t\t\t**\t\t\t**\n\t\t**\t\t\t\t\t**\n\t\t**\t\t\t\t\t**\n\t\t**\t\t\t\t\t**\n\t\t**\t\t\t\t\t**\n\t\t\t**\t\t\t**\n\t\t\t\t******")
