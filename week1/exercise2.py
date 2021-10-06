40489b55a2dd48f79065f5ca1a545b807b5fdaa6
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-10-06 20:06:41 +0100

    Create exercise2.py

---
+++ week1/exercise2.py
@@ -0,0 +1,6 @@
+#1
+print("I am learning how to use the newline and tab special characters")
+#2
+print("My favourite animal\nis the giraffe!")
+#3
+print("I\tknow\thow\tto\tuse\tthe\ttab\tspecial\tcharacter!")
