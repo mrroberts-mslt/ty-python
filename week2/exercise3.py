9d206484efef176819154515040811d96ee6714a
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-10-06 20:24:04 +0100

    Create exercise3.py

---
+++ week2/exercise3.py
@@ -0,0 +1,22 @@
+#1
+name = "Sally"
+age = "sixteen"
+town = "Dublin"
+print("Hi "+ name + ", you are "+ age + " years old and you live in "+ town)
+
+#2
+x = 10
+x += 1
+print(x)
+x = x + 17
+print(x)
+
+#3
+house_width = 14
+house_length = 8
+print(house_length * 2+ house_width * 2)
+
+#3B
+house_width = 18
+house_length = 12
+print(house_length * 2+ house_width * 2)
