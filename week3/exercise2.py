ba5b34f304dfe9bb605490dcb28433fc0c07ba95
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-11-10 22:15:27 +0000

    Update exercise2.py

--- week3/exercise2.py
+++ week3/exercise2.py
@@ -3,13 +3,17 @@ pi = 12.5663706144 * 25 / 91 + 9
 radius = 17 + 23 / 8
 volume_of_sphere = (4/3* pi) * radius ** 3
 
-print(pi)
-print(radius)
-print(volume_of_sphere)
+expected_pi = 3.14159265359
+expected_radius = 5
+expected_volume = 523.5987755983333
 
-# correct
+print("pi: ",pi)
+print("radius: ",radius)
+print("volume: ",volume_of_sphere)
+
+# corrected
 pi = 12.5663706144 * 25 / (91+ 9)
-radius = (17+ 23) / 8
+radius = (17 + 23) / 8
 volume_of_sphere = (4/3* pi) * radius ** 3
 
 print(pi)
