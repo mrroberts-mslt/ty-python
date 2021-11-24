2f93c25a9f3129764e41e8adb8e2ec7350e780e9
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-11-24 17:39:01 +0000

    Create stringIndexing.py

---
+++ week4/stringIndexing.py
@@ -0,0 +1,15 @@
+#Guess which letter comes out?
+myString= "Python"
+print(myString[3])
+
+#Slice the string guess which letters come out?
+myString= "Python"
+print(myString[0:2])
+
+#Slice the string guess which letters come out?
+myString= "Python"
+print(myString[1:])
+
+#Slice the string guess which letters come out?
+myString= "Python"
+print(myString[:5])
