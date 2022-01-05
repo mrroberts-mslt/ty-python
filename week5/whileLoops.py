e048db17076476a8ba9530d1606f68c5419ab598
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2022-01-05 17:34:23 +0000

    Create whileLoops.py

---
+++ week5/whileLoops.py
@@ -0,0 +1,31 @@
+#Loop example 1
+while True:
+     print("This goes on forever")
+
+#Loop example 2    
+counter = 0
+while counter <=10:
+    print("*" * counter)
+    counter +=1
+    
+#Loop example 3 
+startNum = int(input("Enter a start number: "))
+endNum = int(input("Enter a end number: "))
+
+counter = startNum
+total = 0
+while counter <= endNum:
+    print(counter)
+    counter +=1
+    total += counter
+print("The total is: ",total)
+
+#Loop example 4 
+myLetter = input("Enter a letter: ")
+
+while myLetter != "z":
+    print("Letter entered is", myLetter)
+    myLetter = input("Enter a letter: ")
+print("finished")
+
+    
