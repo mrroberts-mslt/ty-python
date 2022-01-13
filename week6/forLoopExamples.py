fc5b17321592ff26bf3ae2a53d63bc4822963d81
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2022-01-13 08:59:54 +0000

    Update forLoopExamples.py

--- week6/forLoopExamples.py
+++ week6/forLoopExamples.py
@@ -1,3 +1,4 @@
+#Example 1
 startNumber = int(input("Please enter a start value: "))
 endNumber = int(input("Please enter an end value: "))
 total = 0
@@ -6,3 +7,10 @@ for i in range(startNumber, endNumber+1):
 	print("The value of i is:",i)
 	total += i
 print("Total is :",total)
+
+#Example 2
+inputName = input("Please enter a name: ")
+
+for letter in inputName:
+	print("The letter is:",letter)
+
