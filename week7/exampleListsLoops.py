ec369cc28908e088a6808245431bdf94aa9ab49d
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2022-01-27 08:20:07 +0000

    Create exampleListsLoops.py

---
+++ week7/exampleListsLoops.py
@@ -0,0 +1,45 @@
+# Task 1: copy to Python Predict Run Investigate Modify Make
+names = ['john','mary','Ellen','Barry']
+print (names[0])
+print (names[-1])
+print (names[2])
+
+###Task2 uncomment when ready
+# for student in names:
+#     print (student)
+
+
+###Task3 uncomment when ready
+# addName = input("Enter a name: ")
+# names.append(addName)
+# print("You added: ",names[-1], "to the list")
+
+###Task4 Enter 3 more names uncomment when ready
+# for i in range (0,3):
+#     addName = input("Enter a name: ")
+#     names.append(addName)
+#     print("You added: ",names[-1], "to the list")
+# print(names)
+
+###Task5 uncomment when ready
+# ages = [1, 6, 32, 6, 4, 40, 50]
+# total = 0
+# for i in ages:
+#     total += i
+# 
+# print("The sum is", total)
+# print("The avg is", total/len(ages))
+
+###Task6 uncomment when ready
+# for i in ages:
+#     if i > 30:
+#         print(i)
+        
+        
+###Task7 uncomment when ready
+# i = 0
+# while i < len(names):
+#   print(names[i])
+#   i += 1
+    
+    
